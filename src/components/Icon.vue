bdca76b3c133d6f5065c5758faa8a5e04f337903
wangkai <wk.dong.up@gmail.com>
2020-08-08 22:25:29 +0800

    移除了Vue-cli中commit检查，解决了vuepress中icon不显示问题

--- src/components/Icon.vue
+++ src/components/Icon.vue
@@ -1,18 +1,19 @@
 <template>
     <svg class="icon">
-        <use :xlink:href="'#'+name"/>
+        <use :xlink:href="`#i-${name}`"/>
     </svg>
 </template>
 
 <script lang="ts">
-  const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
-  try {
-    importAll((require.context("../assets/icon", true, /\.svg$/)));
-  } catch (error) {
-    console.log(error);
-  }
+  // const importAll = (requireContext: __WebpackModuleApi.RequireContext) => requireContext.keys().forEach(requireContext);
+  // try {
+  //   importAll((require.context("../assets/icon", true, /\.svg$/)));
+  // } catch (error) {
+  //   console.log(error);
+  // }
 
   import {Component, Prop, Vue} from "vue-property-decorator";
+  import "./Svg.js";
 
   @Component
   export default class Icon extends Vue {
@@ -24,7 +25,7 @@
     .icon {
         width: 1em;
         height: 1em;
-        fill:black;
+        fill: black;
         margin-right: 0.1em;
         margin-left: 0;
     }
